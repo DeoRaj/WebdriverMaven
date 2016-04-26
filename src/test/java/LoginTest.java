751b73ac4c5bba4972583d3677e62e1e3c2ed1e0
Raj <deo-raj@MERCER.COM>
2016-04-26 18:08:59 +0530

    My Commit

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,35 @@
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+	public static WebDriver driver;
+@BeforeSuite
+	public void setup(){
+		System.setProperty("webdriver.chrome.driver", "C:\\jar\\chromedriver.exe");
+		driver = new ChromeDriver();
+			
+	}
+@Test
+	public void doLogin (){
+		driver.get("http://gmail.com");
+		driver.findElement(By.xpath("//*[@id='Email']")).sendKeys("devraj.gemini");
+		driver.findElement(By.xpath("//*[@id='next']")).click();
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		driver.findElement(By.xpath("//*[@id='Passwd']")).sendKeys("apple");
+		driver.findElement(By.xpath("//*[@id='signIn']")).click();
+		
+	}
+
+@AfterSuite
+	public void tearDown(){
+		driver.quit();
+	}
+	
+}
